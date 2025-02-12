aa5217273a1420a0dcfab66e0ad2e82a2614011f
IrishMan05 <grigodssf65@gmail.com>
2025-02-12 11:46:45 +0300

    Error from Docker console was fixed

--- lab4backend/src/main/java/se/ifmo/lab4backend/Lab4BackendApplication.java
+++ lab4backend/src/main/java/se/ifmo/lab4backend/Lab4BackendApplication.java
@@ -3,9 +3,11 @@ package se.ifmo.lab4backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"se.ifmo", "se.ifmo.lab4backend"})
+@EnableTransactionManagement
 public class Lab4BackendApplication {
 
     public static void main(String[] args) {
